0f35fdecb50f46e6af6e8f5b9944ba684289c9b7
rupa begum <47551411+rupa17@users.noreply.github.com>
2022-04-16 22:27:23 +0600

    Add files via upload

---
+++ loop.py
@@ -0,0 +1,14 @@
+
+
+a = int (input("enter your valu:"))
+print(a, type(a))
+l = [1,2,3,"ddg","gg",True,False]
+print(l[0])
+print(l[5])
+print(l[-5:-1])
+print(l[-4:-1])
+nesl =[[1,2,3],[3,2,4]]
+print(nesl[0])
+l.reverse()
+print(l)
+print(l.remove(3),"remove")
